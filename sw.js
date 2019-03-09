cf5f39d38b66a0f369498754d020c69e87833c82
Rodolfo Marques <rodolfomarques87@gmail.com>
2019-03-09 17:53:47 -0300

    boilerplate PWA
    Arquivos básicos para a construção do PWA do Pontos de Vida. O script do cache ainda não funciona. É preciso estudar a documentação para entender o problema.

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = 'static-v1';
+var pagesToCache = [
+    '/',
+    './index.html',
+    './style.css',
+    './manifest.json',
+]
+
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) =>{
+            console.log("Cache aberto")
+            return cache.addAll(pagesToCache);
+        })
+    )
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+        .then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.filter((key) => {
+                return key.indexOf(CACHE_NAME) !== 0;
+                })
+                .map((key) => {
+                    return caches.delete(key);
+                })
+            );
+        })
+    );
+});
