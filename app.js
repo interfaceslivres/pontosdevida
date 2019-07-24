10c46aa3af2eca5a465a2c3469f5a4119d5e580d
Rodolfo Marques <rodolfomarques87@gmail.com>
2019-07-23 21:20:11 -0300

    Iniciando a implementação do sistema de escolha de cartas
    O posicionamento das cartas funciona.

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+
+// // Este script programa o modal sobre novo level
+
+// let newLevelDialog = document.getElementById("newLevelDialog");
+// let showLevelDialogButton = document.getElementById("itemPositionButton");
+
+// if(!newLevelDialog.showModal) {
+//     dialogPolyfill.registerDialog(newLevelDialog);
+// };
+
+// showLevelDialogButton.addEventListener('click', ()=> {
+//     newLevelDialog.showModal();
+// });
+
+// newLevelDialog.querySelector('.close').addEventListener('click', ()=> {
+//     newLevelDialog.close();
+// });
+
+// // Este script programa o modal sobre nova recompensa
+
+// let newItemDialog = document.getElementById("newItemDialog");
+// let showItemDialogButton = document.getElementById("itemShareButton");
+
+// if(!newItemDialog.showModal) {
+//     dialogPolyfill.registerDialog(newItemDialog);
+// };
+
+// showItemDialogButton.addEventListener('click', ()=> {
+//     newItemDialog.showModal();
+//     Iniciar();
+//     function stop() {
+//         clearInterval(myvar);
+//     }
+
+//     setTimeout(stop, 1200)
+// });
+
+// newItemDialog.querySelector('.close').addEventListener('click', ()=> {
+//     newItemDialog.close();
+// });
