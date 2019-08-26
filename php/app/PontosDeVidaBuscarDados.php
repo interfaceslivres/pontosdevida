062308bb75968c9323e7ef22f817796a4777f3ba
Mateus Danton <mateusdanton@gmail.com>
2019-08-26 17:56:24 -0300

    PHP - Atualizar dados de usuario função incompleta

--- php/app/PontosDeVidaBuscarDados.php
+++ php/app/PontosDeVidaBuscarDados.php
@@ -21,7 +21,7 @@ class PontosDeVidaBuscarDados {
     }
 
     // retornar todos os dados da tabela usuarios
-    public function todosUsuarios() {
+     public function todosUsuarios() {
         $stmt = $this->pdo->query('SELECT nome, login_usuario, senha, email, biografia, data_nascimento, tipo_sangue, nivel, oauth, smtoggle, privacidade '
                 . 'FROM usuario '
                 . 'ORDER BY nome');
@@ -43,6 +43,35 @@ class PontosDeVidaBuscarDados {
         }
         return $DadosUsuarios;
     }
+
+
+    public function dadosUsuarioAtivo($usuarioAtivo) {
+        $stmt = $this->pdo->prepare('SELECT nome, login_usuario, senha, email, biografia, data_nascimento, tipo_sangue, nivel, oauth, smtoggle, privacidade '
+                . 'FROM usuario '
+                . 'WHERE login_usuario = :usuario');
+
+        $stmt->bindValue(':usuario', $usuarioAtivo);
+        $stmt->execute();
+        $DadosUsuario = [];
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $DadosUsuario[] = [
+                'nome' => $row['nome'],
+                'login_usuario' => $row['login_usuario'],
+                'senha' => $row['senha'],
+                'email' => $row['email'],
+                'biografia' => $row['biografia'],
+                'data_nascimento' => $row['data_nascimento'],
+                'tipo_sangue' => $row['tipo_sangue'],
+                'nivel' => $row['nivel'],
+                'oauth' => $row['oauth'],
+                'smtoggle' => $row['smtoggle'],
+                'privacidade' => $row['privacidade']
+            ];
+        }
+        return $DadosUsuario;
+    }
+
+   
 
 
 }
--- php/site.php
+++ php/site.php
@@ -22,6 +22,88 @@ $logado = $_SESSION['username'];
   <?php
   echo" Bem vindo $logado";
   ?>
+
+  <hr>
+
+  <?php 
+    require 'vendor/autoload.php';
+    use PontosDeVida\Connection as Connection;
+    use PontosDeVida\PontosDeVidaBuscarDados as PontosDeVidaBuscarDados;
+    try{
+      $pdo = Connection::get()->connect();
+      $pegarDados = new PontosDeVidaBuscarDados($pdo);
+      $DadosUsuario = $pegarDados->dadosUsuarioAtivo($_SESSION['username']);
+      } catch (\PDOException $e) {
+          echo $e->getMessage();
+      }
+   ?>
+<?php foreach ($DadosUsuario as $dado) : ?>
+    <p>Nome: <?php echo htmlspecialchars($dado['nome']) ?></p>
+    <p>Login: <?php echo htmlspecialchars($dado['login_usuario']); ?></p>
+    <p>Senha: <?php echo htmlspecialchars($dado['senha']); ?></p>
+    <p>Email:<?php echo htmlspecialchars($dado['email']) ?></p>
+    <p>Bio:<?php echo htmlspecialchars($dado['biografia']); ?></p>
+    <p>Data de Nascimento: <?php echo htmlspecialchars($dado['data_nascimento']); ?></p>
+    <p>Tipo Sanguineo: <?php echo htmlspecialchars($dado['tipo_sangue']) ?></p>
+    <p>Nivel: <?php echo htmlspecialchars($dado['nivel']); ?></p>
+    <p>Oauth: <?php echo htmlspecialchars($dado['oauth']); ?></p>
+    <p>Smtoggle: <?php echo htmlspecialchars($dado['smtoggle']) ?></p>
+    <p>Privacidade: <?php echo htmlspecialchars($dado['privacidade']); ?></p>
+    <?php endforeach; ?>
+
+    <hr>
+
+    <?php foreach ($DadosUsuario as $dado) : ?>
+    <h1><a>Atualizar Dados Usiario</a></h1>
+      <form id="form"  method="post" action="">  
+        <div>
+          <input name="F_nome" type="text" value="<?= $dado['nome'] ?>"/> 
+        </div> 
+        <div>
+          <input name="F_login" type="text" value="<?= $dado['login_usuario'] ?>"/> 
+        </div> 
+        <div>
+          <input name="F_senha" type="text" value="<?= $dado['senha'] ?>"/> 
+        </div> 
+        <div>
+          <input name="F_email" type="text" value="<?= $dado['email'] ?>"/> 
+        </div>  
+        <div>
+          <input name="F_biografia" type="text" value="<?= $dado['biografia'] ?>"/> 
+        </div> 
+        <div>
+          <input name="F_data_nascimento" type="text" value="<?= $dado['data_nascimento'] ?>"/> 
+        </div> 
+        <div>
+          <select id="tipo_sanguineo" name="F_tipo_sanguineo" value="Tipo sanguíneo"> 
+            <!-- JS colocar selected na opção slecionada -->
+            
+            <option value="A+" >A+</option>
+            <option value="A-" >A-</option>
+            <option value="B+" >B+</option>
+            <option value="B-" >B-</option>
+            <option value="AB+" >AB+</option>
+            <option value="AB-" >AB-</option>
+            <option value="O+" >O+</option>
+            <option value="O-" >O-</option>
+          </select>
+        </div> 
+
+        <div>
+          <input id="privacidade" type="checkbox" name="F_privacidade" value="1"> PRIVACIDADE <br>
+        </div>
+
+        <script type="text/javascript">
+              $('#tipo_sanguineo').val("<?= $dado['tipo_sangue'] ?>");
+              var privacidade = "<?= $dado['privacidade'] ?>";
+              if ( privacidade == 0) {$("#privacidade").prop("checked", false);}else {$("#privacidade").prop("checked", true);};
+            </script>
+
+          <input type="submit" name="submit" />
+      </form> 
+      <?php endforeach; ?>
+
+  <hr>
    
    <a href="logout.php"><button > Deslogar </button>
 
