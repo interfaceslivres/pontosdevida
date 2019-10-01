c90e46015820da35881ab489555690772f9add07
interfaceslivres <paulohsms@gmail.com>
2019-10-01 18:13:25 -0300

    connection corrigido para mysql

--- php/app/Connection.php
+++ php/app/Connection.php
@@ -1,44 +1,41 @@
 <?php
- 
+
 namespace PontosDeVida;
- 
+
 /**
  * Represent the Connection
  */
 class Connection {
- 
+
     /**
      * Connection
-     * @var type 
+     * @var type
      */
     private static $conn;
- 
+
     /**
      * Connect to the database and return an instance of \PDO object
      * @return \PDO
      * @throws \Exception
      */
     public function connect() {
- 
+
         // read parameters in the ini configuration file
         $params = parse_ini_file('database.ini');
         if ($params === false) {
             throw new \Exception("Error reading database configuration file");
         }
         // connect to the postgresql database
-        $conStr = sprintf("mysql:host=%s;port=%d;dbname=%s;user=%s;password=%s", 
-                $params['host'], 
-                $params['port'], 
-                $params['database'], 
-                $params['user'], 
-                $params['password']);
- 
-        $pdo = new \PDO($conStr);
+        $conStr = sprintf("mysql:host=%s;port=%d;dbname=%s;",
+                        $params['host'],
+                        $params['port'],
+                        $params['database']);
+        $pdo = new \PDO($conStr, $params['user'], $params['password']);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
- 
+
         return $pdo;
     }
- 
+
     /**
      * return an instance of the Connection object
      * @return type
@@ -47,20 +44,20 @@ class Connection {
         if (null === static::$conn) {
             static::$conn = new static();
         }
- 
+
         return static::$conn;
     }
- 
+
     protected function __construct() {
-        
+
     }
- 
+
     private function __clone() {
-        
+
     }
- 
+
     private function __wakeup() {
-        
+
     }
- 
+
 }
