6442e5624103b0dba559ee7d7efd84ac751c0b29
Interfaces Livres <paulohsms@gmail.com>
2019-10-02 10:44:24 -0300

    cadastro

--- cadastro.php
+++ cadastro.php
@@ -3,7 +3,32 @@ Tela: Cadastro
 Aplicativo: Pontos de Vida
 Desenvolvido por: Interfaces Livres
 -->
+<?php
 
+require 'vendor/autoload.php';
+use PontosDeVida\Connection as Connection;
+use PontosDeVida\PontosDeVidaEnviarDados as PontosDeVidaEnviarDados;
+function cadastrar($nome, $login, $senha, $email, $biografia, $data_nascimento, $tipo_sanguineo) {
+
+   try {
+    // connect to the PostgreSQL database
+	  $pdo = Connection::get()->connect();
+    //
+    $InserirDados = new PontosDeVidaEnviarDados($pdo);
+
+    // inserir dados do usuario na tabela usuario
+    $InserirDados->AdicionarDados($nome, $login,  $senha, $email, $biografia, $data_nascimento, $tipo_sanguineo);
+
+	} catch (\PDOException $e) {
+	    echo $e->getMessage();
+	}
+
+	header('Location: ' . $_SERVER['HTTP_REFERER']);
+	exit;
+
+	}
+	
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -28,6 +53,16 @@ Desenvolvido por: Interfaces Livres
 			<div class="mdl-layout-spacer"></div>
 			<div class="demo-card-square mdl-card mdl-cell mdl-cell--4-col">
 				<div class="mdl-card__supporting-text">
+
+					<?php
+		      if( isset($_POST['LoginButton']) )
+		      {
+						$nome = $_POST['F_nome']; $login = $_POST['F_login']; $senha = $_POST['F_senha']; $email = $_POST['F_email']; $biografia = $_POST['F_biografia']; $data_nascimento = $_POST['F_data_nascimento']; $tipo_sanguineo = $_POST['F_tipo_sanguineo'];
+						cadastrar($nome, $login, $senha, $email, $biografia, $data_nascimento, $tipo_sanguineo);
+
+		          //then you can use them in a PHP function.
+		      }
+		      ?>
 				<form action="#">
 					<div id="caixalogin" class="mdl-textfield mdl-js-textfield">
 							<input class="mdl-textfield__input" type="text" id="user" pattern="[A-Z,a-z,0-9,_,-,., ]*">
