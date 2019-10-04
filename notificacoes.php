9628b7406778934edc3531113e02182132169171
Interfaces Livres <paulohsms@gmail.com>
2019-10-04 11:49:45 -0300

    Create notificacoes.php

---
+++ notificacoes.php
@@ -0,0 +1,99 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="mdl/material.min.css">
+	<script src="mdl/material.min.js" id="mdl-script"></script>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+    <div class="mdl-grid">
+        <div class="mdl-layout-spacer"></div>
+        <a id="categoria-notificacoes">
+            <span class="pontos">.</span><span>Notificações</span>
+        </a>
+        <div class="mdl-layout-spacer"></div>
+    </div>
+
+    <div id="notificacoes-amigos" class="mdl-grid">
+        <div class="mdl-layout-spacer"></div>
+
+            <div class="solicitacao_amizade" class="mdl-grid">
+                <div class="chat_imagem"></div>
+                <div class="solicitacao_amizade_desc">
+                    <span>O <b>Hemocentro da Paraíba</b> está precisando urgentemente de doações de sangue do tipo <b>O+</b>. Salve uma vida hoje!
+                    </span>
+                </div>
+            </div>
+
+            <div class="solicitacao_amizade" class="mdl-grid">
+                <div class="chat_imagem"></div>
+                <div class="solicitacao_amizade_desc">
+                    <span>O clã <b>Doadores DEMID</b> fez 5.000 doações! Aqui vai uma recompensa pelo esforço:
+                    </span>
+
+                    <div class="mdl-grid">
+                        <div id="amizade-buttons" class="mdl-cell mdl-cell--1-col">
+                            <button class="mdl-button" id="amizade-button" onclick="">
+                                Abrir
+                            </button>
+
+                            <button class="mdl-button" id="amizade-button">
+                                Guardar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="solicitacao_amizade" class="mdl-grid">
+                <div class="chat_imagem"></div>
+                <div class="solicitacao_amizade_desc">
+                    <span id="solicitacao_amigo_user">Luke Skywalker</span>
+                    <span id="chat_mensagem">
+                        enviou uma solicitação de amizade.
+                    </span>
+
+                    <div class="mdl-grid">
+                        <div id="amizade-buttons" class="mdl-cell mdl-cell--1-col">
+                            <button class="mdl-button" id="amizade-button" onclick="">
+                                Aceitar
+                            </button>
+
+                            <button class="mdl-button" id="amizade-button">
+                                Recusar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="solicitacao_amizade" class="mdl-grid">
+                <div class="chat_imagem"></div>
+                <div class="solicitacao_amizade_desc">
+                    <span id="solicitacao_amigo_user">Nathália Clementino</span>
+                    <span id="chat_mensagem">
+                        enviou uma solicitação de amizade.
+                    </span>
+
+                    <div class="mdl-grid">
+                        <div id="amizade-buttons" class="mdl-cell mdl-cell--1-col">
+                            <button class="mdl-button" id="amizade-button" onclick="">
+                                Aceitar
+                            </button>
+
+                            <button class="mdl-button" id="amizade-button">
+                                Recusar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="mdl-layout-spacer"></div>
+    </div>
+    <div id="bottom-space" class="mdl-grid"></div>
+</body>
+</html>
