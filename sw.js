cdde7da8f8ed2aadbd337b576aca5b0a188f8b46
Nathália Clementino <nclementino.ufpb@gmail.com>
2019-10-06 04:10:56 -0300

    Bug no Cache (Teste)

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ var CACHE_NAME = 'devCach-v10';
 var pagesToCache = [
     '/',
     './home.php',
+    './editarperfil.php',
     './style.css',
     './mdl/material.min.css',
     './manifest.json',
