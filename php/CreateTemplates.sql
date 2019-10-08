1bae0c853dcffbf463e0754999abf54a53a9e71d
Interfaces Livres <paulohsms@gmail.com>
2019-10-08 01:33:24 -0300

    retirada das figurinhas 6 e create figurinhas

--- php/CreateTemplates.sql
+++ php/CreateTemplates.sql
@@ -1,12 +1,84 @@
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("informacao", "Você sabia que os alimentos que você ingere podem interferir na sua aptidão para doar sangue? Nas quatro horas que antecedem a doação é importante estar bem alimentado, porém evitar refeições que sejam gordurosas. Cuidado também com as bebidas alcoólicas: elas não devem ser consumidas nas últimas 12 horas!","01informacao.jpg",3);
 
 INSERT INTO template(nome,descricao,imagem,tipo)
-VALUES("informacao", "Depois de doar sangue, é crucial que você aumente o volume de líquidos ingeridos, sobretudo nas primeiras 24 horas. Capriche na ingestão de água mineral ou outra bebida de sua preferência para auxiliar na reposição do volume perdido na doação!","03water.jpg",3);
+VALUES("educacao", "Você sabia que existe um tipo muito raro de sangue no mundo? Ele é chamado de RH nulo ou sangue dourado, e acontece quando seus glóbulos vermelhos não possuem nenhum tipo de antígeno. Até hoje, só 43 pessoas foram identificadas com este tipo sanguíneo no planeta inteiro, e estas pessoas podem doar para qualquer pessoa com os tipos tradicionais dentro do sistema Rh e Rh negativo.","01educacao.jpg",5);
 
 INSERT INTO template(nome,descricao,imagem,tipo)
-VALUES("educacao", "dasdasdasdasdasdasdas!","03gaspar.jpg",5);
+VALUES("inspiracao", "Salvar vidas não é missão exclusiva dos médicos ou enfermeiros: ao doar uma bolsa de sangue, você mesmo pode salvar a vida de até quatro pessoas!","01inspiracao.jpg",9);
 
 INSERT INTO template(nome,descricao,imagem,tipo)
-VALUES("inspiracao", "uuuuuuuuuuu uuuuuuuuuuuu uuuuuuuu","03obsequio.jpg",9);
+VALUES("entretenimento", "Nas filmagens dos filmes Kill Bill 1 & 2 de Quentin Tarantino, 1700 litros de sangue falso foram usados. Se fosse real, esta quantidade seria o suficiente para salvar 17000 vidas.","01entretenimento.jpg",16);
+
+
 
 INSERT INTO template(nome,descricao,imagem,tipo)
-VALUES("Nosferaaato!?", "Os vampiros da Saga Crepúsculo precisam de sangue para sobreviver, e os pacientes dos hospitais locais também. Seja você do time Jacob ou do time Edward, todos os fãs concordam sobre a importância de salvar vidas!","03vampirinho.jpg",16);
+VALUES("informacao", "Como têm sido suas noites de sono? Um dos pré-requisitos para doar sangue é ter repousado bem na véspera da doação. O motivo é simples: com uma noite bem dormida, diminuem as chances de sentirmos tontura, mal-estar e pressão baixa após doar. Por isso, esteja bem descansado antes de visitar o Hemocentro local!","02informacao.jpg",3);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("educacao", "Por que pessoas com peso abaixo de 50kg não podem doar? Isto acontece porque volume a ser doado é proporcional ao peso do doador. Matematicamente falando, pessoas com menos de 50kg só poderiam doar um volume inferior a 400ml de sangue, que é a quantidade mínima requerida para o volume de anticoagulante dentro da bolsa. Assim, doando-se menos de 400ml, haveria excesso de solução anticoagulante, o que poderia lesar os glóbulos vermelhos.","02educacao.jpg",5);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("inspiracao", "Doar sangue: uma decisão simples com um impacto incalculável!","02inspiracao.jpg",9);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("entretenimento", "O indiano Sr. Mithani realizou 151 doações de sangue durante sua vida. Em 2019 ele completou 65 anos, de forma que não seria mais seguro continuar suas doações. Sua história ficou conhecida e 350 mil pessoas prometeram assumir a sua missão e dar continuidade às doações. Valeu, Mithani! A gente assume a partir daqui!","02entretenimento.jpg",16);
+
+
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("informacao", "Depois de doar sangue, é crucial que você aumente o volume de líquidos ingeridos, sobretudo nas primeiras 24 horas. Capriche na ingestão de água mineral ou outra bebida de sua preferência para auxiliar na reposição do volume perdido na doação!","03informacao.jpg",3);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("educacao", "dasdasdasdasdasdasdas!","03educacao.jpg",5);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("inspiracao", "uuuuuuuuuuu uuuuuuuuuuuu uuuuuuuu","03inspiracao.jpg",9);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("entretenimento", "Os vampiros da Saga Crepúsculo precisam de sangue para sobreviver, e os pacientes dos hospitais locais também. Seja você do time Jacob ou do time Edward, todos os fãs concordam sobre a importância de salvar vidas!","03entretenimento.jpg",16);
+
+
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("informacao", "Você sabia que o doador de sangue tem direito a um dia de folga no trabalho em cada 12 meses trabalhados? Isto está previsto no Decreto-lei nº 5.452 da CLT (Consolidação das Leis do Trabalho). Por essa razão, é emitido um comprovante logo após a doação. Em posse dele, é só repousar depois de ter ido salvar vidas!","04informacao.jpg",3);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("educacao", "Você já se perguntou por que homens podem doar sangue a cada dois meses e as mulheres, apenas a cada três meses? O motivo é a reposição dos estoques de ferro: nas mulheres ela demora mais a acontecer devido às perdas durante os ciclos menstruais.","04educacao.jpg",5);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("inspiracao", "Não é apenas sangue. É a esperança de viver uma vida plena, de realizar sonhos, de construir vitórias!","04inspiracao.jpg",9);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("Nosferaaato!?", "Untold, um famoso festival de música na Transilvânia (terra do Drácula), lançou em 2015 uma campanha nacional para encorajar a população mais jovem a doar sangue. O Drácula convida todos a doar.","04entretenimento.jpg",16);
+
+
+
+
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("informacao", "Mamães podem doar sangue? Em caso de gravidez ou suspeita de gestação, a doação não é permitida. Entretanto, a partir de 12 meses após o parto, já é possível voltar a doar. Ah, se for visitar o Hemocentro junto de crianças, é importante levar alguém que as acompanhe enquanto você faz a doação!","05informacao.jpg",3);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("educacao", "Qual é o seu tipo sanguíneo? De uma maneira geral, os diferentes tipos de sangue interagem entre si, podendo causar incompatibilidade e rejeição. Apesar disso, quem tem sangue AB+ pode receber doações de todos os demais tipos sanguíneos, e quem tem sangue O- pode doar para todos os demais tipos sanguíneos.","05educacao.jpg",5);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("inspiracao", "Você que doou sangue tem muito a se orgulhar! Este é um gesto altruísta e uma grande lição de generosidade!","05inspiracao.jpg",9);
+
+INSERT INTO template(nome,descricao,imagem,tipo)
+VALUES("entretenimento", "Na cidade de Shenzhen, na China existe desde o ano 2000 uma equipe de doação voluntária de sangue. São doadores regulares, que também doam sangue em situações de emergência e ainda ajudam a recrutar novos doadores.","05entretenimento.jpg",16);
+
+
+
+--
+-- INSERT INTO template(nome,descricao,imagem,tipo)
+-- VALUES("informacao", "Como proceder após a doação? Primeiramente, é crucial permanecer sentado por ao menos 15 minutos após o procedimento. Enquanto aguarda, você pode tomar o lanchinho oferecido pelo Hemocentro. Depois de liberado, evite dobrar o braço por 30 minutos e mantenha o curativo lá por no mínimo 4 horas.","06informacao.jpg",3);
+--
+-- INSERT INTO template(nome,descricao,imagem,tipo)
+-- VALUES("educacao", "Os dois maiores motivos para a rejeição e o descarte de uma bolsa de sangue são resultados sorológicos alterados (principalmente a presença de anticorpos contra as hepatites B e C) e a alta concentração de gordura no sangue (lipemia), fazendo com que o plasma torne-se turvo.","06educacao.jpg",5);
+--
+-- INSERT INTO template(nome,descricao,imagem,tipo)
+-- VALUES("inspiracao", "Viva! Você doou sangue hoje! A humanidade agradece. Agora alimente-se bem e descanse, porque você merece!","06inspiracao.jpg",9);
+--
+-- INSERT INTO template(nome,descricao,imagem,tipo)
+-- VALUES("entretenimento", "O ator especialista em artes marciais Jackie Chan tem pavor de agulha, mas supera o medo por uma causa maior. Ele já fez várias campanhas promovendo a doação. As lutas no cinema podem até ser de mentira, mas a luta de quem precisa de sangue é real!","06entretenimento.jpg",16);
